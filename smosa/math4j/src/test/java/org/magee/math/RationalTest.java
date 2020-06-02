47a06c426057dd2c9ec842616dcce93650681ed8
ACupito <acupito2@gmail.com>
2020-06-02 15:50:04 +0200

    Fixed RationalTest class

--- smosa/math4j/src/test/java/org/magee/math/RationalTest.java
+++ smosa/math4j/src/test/java/org/magee/math/RationalTest.java
@@ -8,6 +8,8 @@ import org.magee.math.Rational;
 
 public class RationalTest {
 
+  //test the add() method
+  //the test is good because add() tries to create a new Rational object with 0 as denominator
   @Test
   public void test01()  throws Throwable  {
       Rational rational0 = new Rational(762L, 762L);
@@ -24,6 +26,8 @@ public class RationalTest {
       }
   }
 
+  //test longValue() method
+  //it computes [(-1) / (-661)] = 0.001512
   @Test
   public void test02()  throws Throwable  {
       Rational rational0 = new Rational((-1L), (-661L));
@@ -32,14 +36,20 @@ public class RationalTest {
       assertEquals(0.0015128593040847202, rational0.doubleValue(), 0.01);
   }
 
+  //test multiply() method
+  //there is a bug with multiply()
+  //it doesn't computes the multiplication between numerators but the division
+  //in fact it should have computed {[(-1431) * (-1431)] / [(-1431) * (-1431)]} = 1
   @Test
   public void test03()  throws Throwable  {
       Rational rational0 = new Rational((-1431L), (-1431L));
       Rational rational1 = rational0.multiply(rational0);
       assertEquals(1L, rational0.longValue());
-      assertEquals(4.8833823E-7F, rational1.floatValue(), 0.01F);
+      assertEquals(1, rational1.floatValue(), 0);
   }
 
+  //As i discovered in test03, multiply() method is buggy
+  //so this test isn't useful at the moment
   @Test
   public void test04()  throws Throwable  {
       Rational rational0 = new Rational(1L, 1L);
@@ -48,6 +58,8 @@ public class RationalTest {
       assertEquals((byte)1, rational1.byteValue());
   }
 
+  //Test pow() method
+  //It computes [(6^5) / ((-2)^5)] = 7776/ (-32)
   @Test
   public void test05()  throws Throwable  {
       Rational rational0 = new Rational(6L, (-2L));
@@ -56,6 +68,8 @@ public class RationalTest {
       assertEquals((byte)13, rational1.byteValue());
   }
 
+  //it test subtract(long integer)
+  //so it computes [(1 * (-1)) + ((-1) * (-1))] / ((-1) * (-1)) = 0/1
   @Test
   public void test06()  throws Throwable  {
       Rational rational0 = new Rational((-1L), (-1L));
@@ -65,22 +79,30 @@ public class RationalTest {
       assertEquals((-1L), rational0.denominator);
   }
 
+  //test abs() method
+  //abs() is buggy, it returns the correct value for numerator but not for the denominators
+  //it returns (+ denominator) if (denominator < 0) but it's not correct it should return (-denominator)
   @Test
   public void test07()  throws Throwable  {
       Rational rational0 = new Rational(0L, (-1802L));
       Rational rational1 = rational0.abs();
-      assertEquals(-0.0, rational1.doubleValue(), 0.01);
-      assertEquals((-1802L), rational1.denominator);
+      assertEquals(0.0, rational1.doubleValue(), 0.01);
+      assertEquals((1802L), rational1.denominator);
   }
 
+  //test negate() method
+  //it computes (-887) / (887)
   @Test
   public void test08()  throws Throwable  {
       Rational rational0 = new Rational(887L, 887L);
       Rational rational1 = rational0.negate();
+      assertEquals(-887, rational1.numerator);
       assertEquals((byte) (-1), rational1.byteValue());
       assertEquals(1, rational0.intValue());
   }
 
+  //test doubleValue() method
+  //it computes 0 / 1090 = 0
   @Test
   public void test09()  throws Throwable  {
       Rational rational0 = new Rational(0L, 1090L);
@@ -89,11 +111,23 @@ public class RationalTest {
       assertEquals(0.0, double0, 0.01);
   }
 
+  //As i discovered in test07(), abs() method is buggy
+  //this test doesn't fail because abd() return the correct value for positive rational
+  //so the test is correct but the abs() method is still buggy
   @Test
   public void test10()  throws Throwable  {
       Rational rational0 = new Rational(1184L, 73L);
       Rational rational1 = rational0.abs();
       assertEquals(1184L, rational0.numerator);
       assertEquals(16.21917808219178, rational1.doubleValue(), 0.01);
+  }
+
+  //Simple test to execute inverse() method which is not tested in this test suite
+  @Test
+  public void testInverse() throws Throwable{
+      Rational rational0 = new Rational(5, 3);
+      Rational rational1 = rational0.inverse();
+      assertEquals(3, rational1.numerator);
+      assertEquals(5, rational1.denominator);
   }
 }
